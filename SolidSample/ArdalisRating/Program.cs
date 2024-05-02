96e28169ca917a5cd4822afe3641bf54dcb95059
jvifdz <javiason@gmail.com>
2024-05-02 12:50:52 +0200

    prueba

--- SolidSample/ArdalisRating/Program.cs
+++ SolidSample/ArdalisRating/Program.cs
@@ -11,6 +11,9 @@ namespace ArdalisRating
             var engine = new RatingEngine();
             engine.Rate();
 
+
+
+
             if (engine.Rating > 0)
             {
                 Console.WriteLine($"Rating: {engine.Rating}");
